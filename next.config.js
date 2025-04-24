8cb3627d5ca0563aac742351b40838683810b7bc
Makon <makon@hey.com>
2025-04-24 17:22:37 -0600

    make pwa

--- next.config.js
+++ next.config.js
@@ -3,8 +3,19 @@
  * for Docker builds.
  */
 import "./src/env.js";
+import withPWA from "next-pwa";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const nextConfig = {
+  // Your existing config can go here
+  reactStrictMode: true, // Assuming you want this
+};
 
-export default config;
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default pwaConfig(nextConfig);
